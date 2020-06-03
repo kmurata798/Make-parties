0cf3a5c6c53f8a33077ee8fca16afed177410231
kmurata798 <kmurata798@gmail.com>
2020-06-03 01:59:36 -0700

    [Add]Basic Bootstrap + templates

--- app.js
+++ app.js
@@ -15,9 +15,9 @@ app.get('/test', (req, res) => {
 
 // OUR MOCK ARRAY OF PROJECTS
 var events = [
-  { title: "I am your first event", desc: "A great event that is super fun to look at and good", imgUrl: "https://charitypaws.com/wp-content/uploads/2018/12/courage-cowardly-dog.jpg" },
-  { title: "I am your second event", desc: "A great event that is super fun to look at and good", imgUrl: "https://lh3.googleusercontent.com/proxy/R2V0YQIJ4E-bGGxZvqcsI0yIH-lvhDp-sRhKda0OySG_xXTuX6LDG7oLjaWOsMj5SPyF-W-2P94C0iKofmNAWiO3nh1BvTKAGi0IBFfROt9DteRopFAyUDB7" },
-  { title: "I am your third event", desc: "A great event that is super fun to look at and good", imgUrl: "https://consequenceofsound.net/wp-content/uploads/2015/03/shiloh.jpg?w=806" }
+  { title: "Courage needs help", desc: "A great event that is super fun to look at and good", imgUrl: "https://charitypaws.com/wp-content/uploads/2018/12/courage-cowardly-dog.jpg" },
+  { title: "Bear watching", desc: "A great event that is super fun to look at and good", imgUrl: "https://lh3.googleusercontent.com/proxy/R2V0YQIJ4E-bGGxZvqcsI0yIH-lvhDp-sRhKda0OySG_xXTuX6LDG7oLjaWOsMj5SPyF-W-2P94C0iKofmNAWiO3nh1BvTKAGi0IBFfROt9DteRopFAyUDB7" },
+  { title: "Dog Day", desc: "A great event that is super fun to look at and good", imgUrl: "https://consequenceofsound.net/wp-content/uploads/2015/03/shiloh.jpg?w=806" }
 ]
 
 // INDEX
