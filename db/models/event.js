64755e0cd85a9fc6fa903e7acc000111545f2c60
kmurata798 <kmurata798@gmail.com>
2020-06-06 18:15:05 -0700

    Users can see single events

--- db/models/event.js
+++ db/models/event.js
@@ -3,6 +3,7 @@ module.exports = (sequelize, DataTypes) => {
   const Event = sequelize.define('Event', {
     title: DataTypes.STRING,
     desc: DataTypes.TEXT,
+    // takesPlaceOn: DATE,
     imgUrl: DataTypes.STRING //add this line (don't forget the comma above!)
   }, {});
   Event.associate = function(models) {
