83c624e04953863e32dcdfa7a828579d8f042c63
kmurata798 <kmurata798@gmail.com>
2020-06-07 01:35:58 -0700

    users can delete RSVPs

--- controllers/rsvps.js
+++ controllers/rsvps.js
@@ -15,5 +15,14 @@ module.exports = (app, models) => {
           console.log(err)
       });
     });
-    // DESTROY  
+
+    // DELETE
+    app.delete('/events/:eventId/rsvps/:id', (req, res) => {
+      models.Rsvp.findByPk(req.params.id).then(rsvp => {
+          rsvp.destroy();
+          res.redirect(`/events/${req.params.eventId}`);
+      }).catch((err) => {
+          console.log(err);
+      });
+    });
   }
